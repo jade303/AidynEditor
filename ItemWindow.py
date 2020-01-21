910144658f258ecb4b228d916ff20d9811a75175
TiberGuy <46498180+TiberGuy@users.noreply.github.com>
2020-01-21 15:53:19 -0600

    Update ItemWindow.py

--- ItemWindow.py
+++ ItemWindow.py
@@ -4,5 +4,5 @@ from tkinter import Toplevel
 class ItemEdit:
     def __init__(self, filename):
         itemwin = Toplevel()
-        itemwin.title("Armor and Shield Edit")
+        itemwin.title("Item Edit")
         filename = filename
